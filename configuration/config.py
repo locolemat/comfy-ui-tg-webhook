963dc38583bcb210dee1b810313b94f57115c6b1
locolemat <122878505+locolemat@users.noreply.github.com>
2024-06-12 01:19:36 +0800

    Added PyYaml as a dependency. Fleshed out the config file

--- configuration/config.py
+++ configuration/config.py
@@ -1,4 +1,12 @@
 import yaml
 import os
 
-CONFIG_FILE_LOCATION = os.path.join(os.path.dirname, 'config.yaml')
+CONFIG_FILE_LOCATION = os.path.join(os.path.dirname(__file__), 'config.yaml')
+
+config = None
+
+with open(CONFIG_FILE_LOCATION) as f:
+    config = yaml.safe_load(f)
+
+BOT_TOKEN = config['bot']
+ADDRESSES = config['addresses']
