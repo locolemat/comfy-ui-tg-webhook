743c70b3e9c6b864546be6e3bf68fc5451a5b47b
locolemat <122878505+locolemat@users.noreply.github.com>
2024-06-12 10:35:35 +0800

    Added JSON-e as a dependency

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+
 from token_reader import settings
+
 
 async def main():
     bot = Bot(token=settings.bot_token.get_secret_value())
     dp = Dispatcher()
-
+   
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
--- token_reader.py
+++ token_reader.py
@@ -3,6 +3,7 @@ from pydantic import SecretStr
 
 class Settings(BaseSettings):
     bot_token: SecretStr
+    language: str
 
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
