9025ba8b631e57b5daeed11d4f172181ec946767
locolemat <122878505+locolemat@users.noreply.github.com>
2024-06-29 23:52:56 +0800

    Added dimensions to image to video generation

--- handlers/generation.py
+++ handlers/generation.py
@@ -182,9 +182,10 @@ async def from_image_generation(message: Message, state: FSMContext):
 
     if server:
         server.busy(True)
+        dimensions = utils.get_dimensions(data["dimensions"])
         await client.upload_image(address=server.address(), image_path=photo_path)
 
-        await client.prompt_query(address=server.address(), prompt=image_name, id = id, workflow=workflow())
+        await client.prompt_query(address=server.address(), prompt=image_name, id = id, workflow=workflow(), width=dimensions["width"], height=dimensions["height"])
 
         start_time = time.time()
         await utils.results_polling(address=server.address(), status_func=client.get, download_func=client.download, id=id, file_type=file_type)
