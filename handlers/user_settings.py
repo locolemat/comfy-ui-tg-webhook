5d03571833addcb4fb6f1ef77f2d1c5120d45c64
locolemat <122878505+locolemat@users.noreply.github.com>
2024-07-02 22:41:29 +0800

    Commands actually work

--- handlers/user_settings.py
+++ handlers/user_settings.py
@@ -22,7 +22,15 @@ model_description_localisation = {
 }
 
 
-@router.callback_query(Command('generate'), StateFilter(None))
+@router.message(Command('generate'), StateFilter(None))
+async def begin_generation_command(message: Message, state: FSMContext):
+    await message.delete()
+
+    await message.answer(
+        text=language.generate_begin_msg,
+        reply_markup=generation_keyboard()
+    )
+
 @router.callback_query(F.data=="generate", StateFilter(None))
 async def begin_generation(call: CallbackQuery, state: FSMContext):
     await call.message.delete()
@@ -33,7 +41,15 @@ async def begin_generation(call: CallbackQuery, state: FSMContext):
     )
 
 
-@router.callback_query(Command('model'), StateFilter(None))
+@router.message(Command('model'), StateFilter(None))
+async def choose_model_command(message: Message, state: FSMContext):
+    await message.delete()
+
+    await message.answer(
+        text=language.model_choice_desc,
+        reply_markup=choose_model_keyboard()
+    )
+
 @router.callback_query(F.data=="choose_model", StateFilter(None))
 async def choose_model(call: CallbackQuery, state: FSMContext):
     await call.message.delete()
