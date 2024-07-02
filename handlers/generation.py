efa68eefe33bfa7a776dd13673d6c645d6520699
locolemat <122878505+locolemat@users.noreply.github.com>
2024-07-02 22:17:19 +0800

    Returned the /start function back to generation.py

--- handlers/generation.py
+++ handlers/generation.py
@@ -7,7 +7,7 @@ from aiogram.types import Message, CallbackQuery, FSInputFile
 from aiogram.fsm.context import FSMContext
 
 from configuration.localisation import LanguageModel, language
-from keyboards import dimensions_keyboard
+from keyboards import dimensions_keyboard, greeting_keyboard
 from workflows.controller import WorkflowTextToVideo, WorkflowTextToImage, WorkflowImageToVideo
 
 from server_queue.server_queue import QueueItem
@@ -31,6 +31,36 @@ greeting_buttons_text = {
     language.button_generate_text_video,
     language.button_generate_image_video
 }
+
+
+@router.message(F.text, CommandStart())
+async def greeting_reply(message: Message, state: FSMContext):
+    await state.clear()
+    
+    tgid = message.from_user.id
+
+    session = create_session()
+
+    user = User.return_user_if_exists(tgid=tgid, session=session)
+
+    if user is None:
+        username = message.from_user.first_name
+        balance = 10
+        preferred_model = "anithing_v11Pruned.safetensors"
+        user = User(username=username, tgid=tgid, balance=balance, preferred_model=preferred_model)
+        session.add(user)
+        session.commit()
+
+    else:
+        username = user.username
+        balance = user.balance
+
+    session.close()
+
+    await message.answer(
+        text=LanguageModel.with_context(template=language.greeting, context={"username":username,"tokens":balance}),
+        reply_markup=greeting_keyboard()
+    )
 
 
 @router.callback_query(F.data.in_(greeting_buttons_text), StateFilter(None))
--- handlers/user_settings.py
+++ handlers/user_settings.py
@@ -21,35 +21,6 @@ model_description_localisation = {
     "realvisxlV40_v40LightningBakedvae.safetensors": language.model_realvisxl_desc
 }
 
-@router.message(F.text, CommandStart())
-async def greeting_reply(message: Message, state: FSMContext):
-    await state.clear()
-    
-    tgid = message.from_user.id
-
-    session = create_session()
-
-    user = User.return_user_if_exists(tgid=tgid, session=session)
-
-    if user is None:
-        username = message.from_user.first_name
-        balance = 10
-        preferred_model = "anithing_v11Pruned.safetensors"
-        user = User(username=username, tgid=tgid, balance=balance, preferred_model=preferred_model)
-        session.add(user)
-        session.commit()
-
-    else:
-        username = user.username
-        balance = user.balance
-
-    session.close()
-
-    await message.answer(
-        text=LanguageModel.with_context(template=language.greeting, context={"username":username,"tokens":balance}),
-        reply_markup=greeting_keyboard()
-    )
-
 
 @router.callback_query(Command('generate'), StateFilter(None))
 @router.callback_query(F.data=="generate", StateFilter(None))
