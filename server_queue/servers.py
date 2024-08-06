e504f8043492e9513a90ff9841991ea9c9d411f0
locolemat <122878505+locolemat@users.noreply.github.com>
2024-08-06 21:47:29 +0800

    Reverted get_server_queue and get_all_servers to returning scalars object again

--- server_queue/servers.py
+++ server_queue/servers.py
@@ -75,7 +75,7 @@ class Server(Base):
     @classmethod
     def get_all_servers(cls):
         with Session(queue_engine) as session:
-            return list(session.query(Server))
+            return session.query(Server)
 
 
     def __repr__(self):
