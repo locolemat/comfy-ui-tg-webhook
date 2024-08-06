c2b8892ac118cfd17dbcffe51391a4a9951ef306
locolemat <122878505+locolemat@users.noreply.github.com>
2024-08-06 20:19:41 +0800

    Minor fix

--- server_queue/__init__.py
+++ server_queue/__init__.py
@@ -1,2 +1,2 @@
-from .server_queue import Server, Queue
-from .servers import NUMBER_OF_IMAGE_SERVERS, NUMBER_OF_VIDEO_SERVERS
+from .server_queue import Queue
+from .servers import Server, NUMBER_OF_IMAGE_SERVERS, NUMBER_OF_VIDEO_SERVERS
