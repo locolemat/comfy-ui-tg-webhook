24c55ae8e81d8b890490c0b2a08d853eaed527f2
locolemat <122878505+locolemat@users.noreply.github.com>
2024-08-06 19:37:23 +0800

    Another approach to the test

--- server_queue/server_queue.py
+++ server_queue/server_queue.py
@@ -140,7 +140,7 @@ class Queue(Base):
     @classmethod
     def get_queue(cls):
         with Session(queue_engine) as session:
-            return session.scalars(select(Queue).order_by(Queue.id))
+            return session.get(Queue).order_by(Queue.id)
 
 
     @classmethod
